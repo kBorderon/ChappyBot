de6921fe08ce753671a33ceb5a1b4ff65e22de85
Kévin Borderon <borderonk@gmail.com>
2020-05-10 22:57:34 +0200

    :construction: Création des évènements guildMemberAdd (nouvel invité dans le discord) et guildMemberRemove (départ d'un utilisateur)

---
+++ evenements/client/guildMemberAdd.js
@@ -0,0 +1,29 @@
+const { MessageEmbed, MessageAttachment } = require("discord.js");
+const image = new MessageAttachment('./contenu/bienvenue.png');
+
+/**
+ * Évènement guildMemberAdd
+ * @author Kévin Borderon
+ * @event Client#guildMemberAdd
+ * @type {object}
+ * @property {Client} client - Référence au bot
+ * @property {GuildMember} member - Référence au membre ayant rejoint le serveur discord
+ */
+module.exports = (client, member) => {
+	const nomServeur = member.guild.name;
+	const utilisateur = member.user;
+
+	const contenu = new MessageEmbed()
+			.setColor("#2e990a")
+			.setTitle(`Bienvenue à ${utilisateur.username}`)
+			.setURL("https://www.cnajep.asso.fr/wp-content/uploads/2018/10/bienvenue.jpg")
+			.setTimestamp()
+			.setDescription(`Bienvenue parmis nous, sur le seveur Discord ${nomServeur} !`)
+			.setThumbnail(utilisateur.displayAvatarURL())
+			.attachFiles(image)
+			.setFooter("Un utilisateur nous a rejoint")
+			.setImage('attachment://bienvenue.png');
+
+	/* On envoie le message préparé, sur le channel ayant l'ID cible */
+		client.channels.cache.get('703623777061175428').send(contenu);
+};
---
+++ evenements/client/guildMemberRemove.js
@@ -0,0 +1,29 @@
+const { MessageEmbed, MessageAttachment } = require("discord.js");
+const image = new MessageAttachment('./contenu/au_revoir.png');
+
+/**
+ * Évènement guildMemberRemove
+ * @author Kévin Borderon
+ * @event Client#guildMemberRemove
+ * @type {object}
+ * @property {Client} client - Référence au bot
+ * @property {GuildMember} member - Référence au membre quittant le serveur discord
+ */
+module.exports = (client, member) => {
+	const nomServeur = member.guild.name;
+	const utilisateur = member.user;
+
+	const contenu = new MessageEmbed()
+			.setColor("#d31d1d")
+			.setTitle(`Au revoir à ${utilisateur.username}`)
+			.setURL("https://img.over-blog-kiwi.com/1/31/67/43/20161128/ob_419371_au-revoir-2.jpg")
+			.setTimestamp()
+			.setDescription(`Au revoir, merci de ta visite sur le seveur Discord ${nomServeur} !`)
+			.setThumbnail(utilisateur.displayAvatarURL())
+			.attachFiles(image)
+			.setFooter("Un utilisateur nous a quitté")
+			.setImage('attachment://au_revoir.png');
+
+	/* On envoie le message préparé, sur le channel ayant l'ID cible */
+		client.channels.cache.get('703623777061175428').send(contenu);
+};
