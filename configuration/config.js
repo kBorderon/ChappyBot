e100c0cbe8d2c7772132c4b7e636266b310800c2
Kévin Borderon <borderonk@gmail.com>
2020-05-16 22:12:43 +0200

    :construction: Préparation du bot pour la plateforme Heroku

--- configuration/config.js
+++ configuration/config.js
@@ -1,3 +1,3 @@
-exports.TOKEN = "NzExMzA0NzE2ODI2NzA2MDQy.XsBEtA.6TdKjh4H5eSC8bFa2LoWW0FZhNQ";
+exports.TOKEN = "NzExMzA0NzE2ODI2NzA2MDQy.XsBIxg.2N1g_Gzv8ipEZmP_EmvZbkm96JQ";
 exports.PREFIX = "?";
 exports.DEFAULT_COOLDOWN = 5;
