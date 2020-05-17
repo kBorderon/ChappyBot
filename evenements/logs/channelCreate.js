c6a7d917882d50230b49071a47b5f4fdb80643f1
Kévin Borderon <borderonk@gmail.com>
2020-05-17 11:40:19 +0200

    :construction: Mise à jour des fichiers help.js, channelCreate.js. Correction des fichiers channelUpdate.js, channelDelete.js

--- evenements/logs/channelCreate.js
+++ evenements/logs/channelCreate.js
@@ -12,6 +12,12 @@ const { MessageEmbed } = require("discord.js");
  */
 module.exports = async (client, channel) => {
 	/* ATTENTION : une erreur peut être générée, si le bot ne dispose pas de droits assez élevés. Vous pouvez attribuer un rôle administrateur, car je ne développerai pas de commandes sensibles disponibles par 	 * l'ensemble des utilisateurs du serveur discord où se situe le bot (faites attention si vous développez des commandes sensibles : kick, ban, mute ..) */
+
+	/* L'envoi d'un message privé, génère une création de channel, pour y remédier on bloque le type dm */
+	if(channel.type === "dm"){
+		return;
+	}
+
 	const journauxAuditGuilde = await channel.guild.fetchAuditLogs({
 		limit: 1, /* On récupère une seule instance de nos logs serveurs (le 1 équivaut à la dernière) */
 		type: 'CHANNEL_CREATE' /* Type de l'évènement à récupérer (valeur : CHANNEL_CREATE = 10) */
@@ -24,12 +30,13 @@ module.exports = async (client, channel) => {
 	const { executor } = dernierChannelCree;
 
 	const contenu = new MessageEmbed()
-			.setColor("#f2e44f")
-			.setTitle("Création d'un nouveau channel")
-			.setTimestamp()
-			.setDescription(`Création du channel **${channel.name}** !`)
-			.setFooter(executor.username, executor.displayAvatarURL());
+		.setColor("#f2e44f")
+		.setTitle("Création d'un nouveau channel")
+		.setTimestamp()
+		.setDescription(`Création du channel **${channel.name}** !`)
+		.setFooter(executor.username, executor.displayAvatarURL());
 
 	/* On envoie le message préparé, sur le channel ayant l'ID cible */
-	client.channels.cache.get('<votre_id_channel>').send(contenu); /* A REMPLACER par l'id de votre salon cible  */
+	client.channels.cache.get('<votre_id_channel>').send(contenu); /* A REMPLACER par l'id de votre salon cible */
 };
+
